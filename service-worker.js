ffd38cd27ea86dee570278c36b4fec9c7dafc7b1
Shafiq R. Khan <37314609+shfqrkhn@users.noreply.github.com>
2025-04-17 12:48:08 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'pmp-prep-v3';
+const urlsToCache = [
+    './',
+    'index.html'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    function(response) {
+                        if(!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        var responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(function(cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    }
+                ).catch(function() {
+                    return caches.match('index.html'); // Fallback to the main page if offline and no cache
+                });
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    var cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
